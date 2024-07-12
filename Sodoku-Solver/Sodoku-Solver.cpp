aa453a1f5522256a5c573d258bf45c1198c0883c
Ali Zahid <ali.zahidd21@gmail.com>
2024-07-12 19:09:13 +0500

    Project Completed

--- Sodoku-Solver/Sodoku-Solver.cpp
+++ Sodoku-Solver/Sodoku-Solver.cpp
@@ -4,10 +4,10 @@
 #include <vector>
 using namespace std;
 
-void print(vector<vector<int>> Sodoku) {
+void Print(vector<vector<int>> Grid) {
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
-            cout << Sodoku[i][j] << " ";
+            cout << Grid[i][j] << " ";
             //if (j == 2 || j == 5 || j == 8) {
             //   cout << "  ";
             //}
@@ -29,13 +29,13 @@ bool CheckSolvedRow(vector<int> row) {
     }
     return true;
 }
-bool CheckSolvedBlock(vector<vector<int>> Sodoku, int Block) {
+bool CheckSolvedBlock(vector<vector<int>> Grid, int Block) {
     vector<int>temp;
     int Row = (Block / 3) * 3;
     int Col = (Block % 3) * 3;
     for (int i = Row; i < Row + 3; i++) {
         for (int j = Col; j < Col + 3; j++) {
-            temp.push_back(Sodoku[i][j]);
+            temp.push_back(Grid[i][j]);
         }
     }
     if (CheckSolvedRow(temp)) {
@@ -43,34 +43,101 @@ bool CheckSolvedBlock(vector<vector<int>> Sodoku, int Block) {
     }
     return false;
 }
-bool CheckSolvedColumn(vector<vector<int>> Sodoku, int Column) {
+bool CheckSolvedColumn(vector<vector<int>> Grid, int Column) {
     vector<int>temp;
     for (int i = 0; i < 9; i++) {
-        temp.push_back(Sodoku[i][Column]);
+        temp.push_back(Grid[i][Column]);
     }
     if (CheckSolvedRow(temp)) {
         return true;
     }
     return false;
 }
-bool CheckSolved(vector<vector<int>> Sodoku) {
+bool CheckSolved(vector<vector<int>> Grid) {
     for (int i = 0; i < 9; i++) {
-        if (!(CheckSolvedRow(Sodoku[i]) && CheckSolvedBlock(Sodoku, i) && CheckSolvedColumn(Sodoku, i))) {
+        if (!(CheckSolvedRow(Grid[i]) && CheckSolvedBlock(Grid, i) && CheckSolvedColumn(Grid, i))) {
             return false;
         }
     }
     return true;
 }
-int main() {
-    ifstream reader("text.txt");
-    vector<vector<int>> Sodoku(9, vector<int>(9));
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            reader >> Sodoku[i][j];
+
+bool isPresent(vector<vector<int>> Grid,int Num,  int i, int j) {
+    for (int a = 0; a < 9; a++) {
+        if (Grid[i][a] == Num || Grid[a][j] == Num) {
+            return true;
         }
     }
-    cout << CheckSolved(Sodoku);
+    int Row = (i / 3) * 3;
+    int Col = (j / 3) * 3;
+    for (int a = Row; a < Row + 3; a++) {
+        for (int b = Col; b < Col + 3; b++) {
+            if (Grid[a][b] == Num) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+bool NotesMaker(vector<vector<vector<int>>>& Notes, const vector<vector<int>>& Grid) {
+    vector<int> temp = { 1,2,3,4,5,6,7,8,9 };
+    Notes = vector<vector<vector<int>>>(9, vector<vector<int>>(9));
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (Grid[i][j] == 0) {
+                for (int k = 0; k < 9; k++) {
+                    if (!isPresent(Grid, temp[k], i, j)) {
+                        Notes[i][j].push_back(temp[k]);
+                    }
+                }
+            }
+        }
+    }
+    return true;
+}
+bool NumberInserter(vector<vector<vector<int>>>& Notes, vector<vector<int>>& Grid, int& Count) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (Grid[i][j] == 0) {
+                if (Notes[i][j].size() == 1) {
+                    Grid[i][j] = Notes[i][j][0];
+                    Notes[i][j].clear();
+                    Count--;
+                }
+            }
+        }
+    }
+    return true;
+}
+bool SolveGrid(vector<vector<int>>& Grid) {
+    if (CheckSolved(Grid)) {
+        return true;
+    }
+    int Count = 0;
+    for (int i = 0; i < 9; i++) {
+        Count = Count + static_cast<int>(count(Grid[i].begin(), Grid[i].end(), 0));
+    }
+    vector<vector<vector<int>>>Notes(9, vector<vector<int>>(9));
+    while (Count != 0) {
+    //for (int a = 0; a < 2; a++) {
+        NotesMaker(Notes, Grid);
+        NumberInserter(Notes, Grid, Count);
+    }
+    return true;
+}
+int main() {
+    ifstream reader("text.txt");
+    vector<vector<int>> Grid(9, vector<int>(9));
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            reader >> Grid[i][j];
+        }
+    }
+    cout << CheckSolved(Grid) << endl;
+    Print(Grid);
+    SolveGrid(Grid);
+    cout << endl;
+    Print(Grid);
     reader.close();
-
     return 0;
 }
